a166c24c7326fb5a41408c047b008cb8436cf906
Daniele Martini <ing.martini@gmail.com>
2020-04-23 11:35:36 +0200

    prima rotta

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+
+		return "Ciao Laravel";
+       
+       //return redirect(route('home')); //effetto un redirect alla root del progetto
+});
+
